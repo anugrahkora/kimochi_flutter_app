9fbe2f550d63a6c936ebf0a5e499fc1d3e5c40dd
Anugrah K <anugrahkorembeth.ak@gmail.com>
2025-07-11 13:33:44 +0530

    Implemented Details view

--- lib/features/details/view/details_view.dart
+++ lib/features/details/view/details_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kimochi_flutter_app/models/university.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DetailsView extends StatelessWidget {
   final University? universityDetails;
@@ -7,6 +8,64 @@ class DetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final name = universityDetails?.name ?? 'University Details';
+    final country = universityDetails?.country ?? 'N/A';
+    final webPages = universityDetails?.webPages?.isNotEmpty == true
+        ? universityDetails!.webPages!.first
+        : null;
+    final stateProvince = universityDetails?.stateProvince ?? 'N/A';
+    final alphaTwoCode = universityDetails?.alphaTwoCode ?? 'N/A';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(universityDetails?.name ?? 'University Details'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 8,
+            children: [
+              _buildValues('Name', name),
+              _buildValues('Country', country),
+              _buildValues(
+                'Website',
+                webPages,
+                onTap: () async {
+                  final url = webPages != null ? Uri.tryParse(webPages) : null;
+                  if (url != null) {
+                    await launchUrl(url);
+                  }
+                },
+              ),
+              const SizedBox(height: 24),
+              Text('Other Details', style: const TextStyle(fontSize: 19)),
+              const Divider(),
+              _buildValues('State Province', stateProvince),
+              _buildValues('Alpha Two Code', alphaTwoCode),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  Row _buildValues(String label, String? value, {Function()? onTap}) => Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Expanded(
+        child: Text(
+          label,
+          style: const TextStyle(fontSize: 18, color: Colors.black54),
+        ),
+      ),
+      Expanded(
+        flex: 2,
+        child: InkWell(
+          onTap: onTap,
+          child: Text(value ?? '', style: const TextStyle(fontSize: 18)),
+        ),
+      ),
+    ],
+  );
 }
